e2775365a6b62d427076b6decdbe0611b8782503
Amy Weitzman <amyweitzman@yahoo.com>
2021-09-26 07:10:29 -0400

    add email notification checkbox to profile page

--- src/components/subcomponents/views/Profile.js
+++ src/components/subcomponents/views/Profile.js
@@ -9,6 +9,12 @@ const Profile = ({email, url}) => {
             <label for="profile-email-field">Email</label>
             <input type="email" class="form-control" id="profile-email-field" disabled value={email} />
           </div>
+          <div class="form-check form-group">
+            <input class="form-check-input" type="checkbox" id="receive-notifications-checkbox" checked disabled />
+            <label class="form-check-label" for="receive-notifications-checkbox">
+              Receive accessibility reports via email
+            </label>
+          </div>
           <div class="form-group">
             <label for="profile-url-field">Website URL</label>
             <input type="url" class="form-control" id="profile-url-field" disabled value={url} />
